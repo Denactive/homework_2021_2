52311fffb3725afcdec33e2cab0640ef514dbeb5
Денис <d.turchin@bk.ru>
2021-08-31 00:37:06 +0300

    fixed #1

--- source/sort.js
+++ source/sort.js
@@ -1,21 +1,33 @@
 // Турчин Денис
+'use strict'
 
-const sort = (sequence) => {
 
-    const firstToUpperAndSort = (word) => {
-        // localeCompare позволяет делать исключение при сортировке буквы "ё", код юникода которой - максимальный среди русских букв
-        let sorted_word = word.toLocaleLowerCase()
-            .split('')
-            .sort( (a, b) => a.localeCompare(b) )
-            .join('');
-
-        return sorted_word.charAt(0)
-            .toLocaleUpperCase()
-            .concat(sorted_word.slice(1))
-    }
-
-    return sequence.split(' ')
-        .map( (el) => firstToUpperAndSort(el) )
+/**
+ * Sorts letters of the given word. Capitalizes the first letter of the sorted letter sequence
+ * 
+ * @param {string} word - Initial word.
+ * @return {string} word - Transformed word 
+ */
+const sortLettersCap = (word) => {
+    let sorted_word = word.toLocaleLowerCase()
+        .split('')
         .sort( (a, b) => a.localeCompare(b) )
-        .join(' ')
+        .join('');
+
+    return sorted_word.charAt(0)
+        .toLocaleUpperCase()
+        .concat(sorted_word.slice(1));
 }
+
+
+/**
+ * Sorts words spelling sequence by letters and sorts result sequences by words. Capitalizes the first letter of each new word
+ * 
+ * @param {string} sequence - Initial sentence.
+ * @return {string} sequence - Transformed sequence 
+ */
+const sort = (sequence) => 
+    sequence.split(' ')
+        .map( (el) => sortLettersCap(el) )
+        .sort( (a, b) => a.localeCompare(b) )
+        .join(' ');
